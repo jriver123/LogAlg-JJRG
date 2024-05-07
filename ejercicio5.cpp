c0b99fbb523dfcfda60f6b831fe812049474cd79
Oalvarado031 <jjrivera@uamv.edu.ni>
2024-05-07 11:36:24 -0600

    sesiones de clase

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,2 +1,19 @@
 /*crear un numero y obtener la tabla de multiplicacion de dicho numero*/
 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    cout <<"Ingresa un numero: ";
+    cin >> num;
+
+    int count =1;
+    while (count <= 12){
+        cout << num << " x " << count << "=" << num * count << endl;
+        count ++;
+    }
+    cout << "Feliz cumpleanos Gabriela!";
+    return 0;
+}
