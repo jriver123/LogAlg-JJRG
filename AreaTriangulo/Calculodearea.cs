119e5d3f41c31fc6de934b745486139e9e28031e
Jonathan Josué Rivera Guido <164054777+jriver123@users.noreply.github.com>
2024-10-20 17:43:32 -0700

    sd

---
+++ AreaTriangulo/Calculodearea.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangulo
+{
+    internal class Calculodearea
+    {
+        public static double calcular_area_triangulo(double[] areas)
+
+        {
+            double basetriangulo = 0;
+            double altura = 0;
+
+            foreach(double area in areas)
+            {
+              basetriangulo += area;
+               altura += area;
+            }
+            return basetriangulo * altura / 2;
+        }
+
+        public static void imprimir_area(double areatriangulo)
+        {
+            Console.WriteLine($"El ara del triángulo es: {areatriangulo}");
+        }
+    }
+}
---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangulo
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area;
+           
+            Console.WriteLine("Programa para calcular área de triángulo utilizando métodos\n");
+            
+            Console.WriteLine("Ingrese la base del triángulo: ");
+            double basetriangulo=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del triángulo: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            area = Calculodearea.calcular_area_triangulo(areas);
+            Calculodearea.imprimir_area(area);
+            Console.ReadKey();
+        }
+            
+    }
+}
